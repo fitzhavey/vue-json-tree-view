5ce35fd58170328578f31236647aaa9c2b9611fd
Arvid Kahl <arvid.kahl@digimondo.de>
2017-02-17 12:40:35 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import TreeViewItem from './components/tree-view/TreeViewItem.vue';
 import TreeView from './components/tree-view/TreeView.vue';
-
-export const components = {
-  TreeViewItem,
-  TreeView
-};
-
-export default {
-  install: (Vue) => {
-    console.log("Installing Components", components)
-    Object.keys(components).forEach((name) => {
-      Vue.component(name, components[name]);
-    });
-  }
-};
-// 
+//
+// export const components = {
+//   TreeViewItem,
+//   TreeView
+// };
+//
+// export default {
+//   install (Vue) {
+//     console.log("Installing Components", components)
+//     Object.keys(components).forEach((name) => {
+//       Vue.component(name, components[name]);
+//     });
+//   }
+// };
+//
 // export {
 //   TreeView
 // };
@@ -25,3 +25,10 @@ export default {
 // 		Vue.component("TreeView", TreeView);
 // 	}
 // };
+
+function install (Vue) {
+		Vue.component("TreeViewItem", TreeViewItem);
+		Vue.component("TreeView", TreeView);
+}
+exports = module.exports = install;
+export default install;
