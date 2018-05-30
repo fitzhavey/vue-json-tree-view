057893ef695e94e57d765246d475b12a65cd7f24
Michael <michael.fitzhavey@gmail.com>
2018-05-30 15:15:51 +0100

    setup demo

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ new Vue({
     },
     data() {
         return {
-          sampleJSON: {"testArray": ["http://www.google.com", "in a Test Array", 0, 1, true, false], "component":"vue-json-tree-view","descripton":"A JSON Tree View built in Vue.js","tags":[{"name":"vue.js"},{"name":"JSON"}],"steps":["HTML Template","Root Component","View Component",{"Transformation Logic":["Transform Objects","Transform Arrays","Transform Values"]},"Animate","Allow Options","Blog about it..."]},
-          sampleJSONString: "http://www.google.com",
-          sampleJSONArray: ["http://www.google.com", "in a Test Array", 0, 1, true, false],
+          sampleJSON: {"testArray": ["Just a Test String", "in a Test Array", "www.google.com" , 0, 1, true, false], "component":"vue-json-tree-view","descripton":"A JSON Tree View built in Vue.js","tags":[{"name":"vue.js"},{"name":"JSON"}],"steps":["HTML Template","Root Component","View Component",{"Transformation Logic":["Transform Objects","Transform Arrays","Transform Values"]},"Animate","Allow Options","Blog about it..."]},
+          sampleJSONString: "Just a Test String",
+          sampleJSONArray: ["Just a Test String", "in a Test Array", 0, 1, true, false],
         };
     },
     methods: {
